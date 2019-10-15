af18166237f9ac45598dcc9d7eed34aa454cee27
lorenzo <lorenzorepetto28@gmail.com>
2019-10-15 11:36:15 -0300

    Se agrego la dependencia Flask-Login

--- flaskps/__init__.py
+++ flaskps/__init__.py
@@ -1,10 +1,15 @@
 from flask import Flask, render_template
 from flaskps.config import Config
-import pymysql
+from flask_login import LoginManager
 
 # Configuración inicial de la app
 app = Flask(__name__)
 app.config.from_object(Config)
 
+
+# Session manager
+login_manager = LoginManager()
+login_manager.init_app(app)
+login_manager.login_view = "login"
 
 from flaskps import routes
