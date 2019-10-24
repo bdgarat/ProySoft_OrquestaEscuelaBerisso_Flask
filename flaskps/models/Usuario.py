c2e672cf92c49765e63064de96c4ca887be2b0f1
lorenzo <lorenzorepetto28@gmail.com>
2019-10-23 21:29:05 -0300

    Se actualizo archivo .sql con usuarios de prueba

--- flaskps/models/Usuario.py
+++ flaskps/models/Usuario.py
@@ -26,7 +26,7 @@ class Usuario(object):
     @classmethod
     def get_usuarios_por_rol(self, rol):
         sql = """
-            SELECT u.id, u.email, u.first_name, u.last_name FROM usuario u
+            SELECT u.id, u.email, u.first_name, u.last_name, u.activo FROM usuario u
             INNER JOIN usuario_tiene_rol ur ON (u.id = ur.usuario_id)
             INNER JOIN rol r ON (ur.rol_id = r.id)
             WHERE r.nombre = %s
--- grupo22.sql
+++ grupo22.sql
@@ -1,11 +1,11 @@
 -- phpMyAdmin SQL Dump
--- version 4.9.1
+-- version 4.9.0.1
 -- https://www.phpmyadmin.net/
 --
 -- Servidor: localhost
--- Tiempo de generación: 10-10-2019 a las 17:11:48
--- Versión del servidor: 10.4.8-MariaDB
--- Versión de PHP: 7.1.32
+-- Tiempo de generación: 24-10-2019 a las 02:26:48
+-- Versión del servidor: 10.4.6-MariaDB
+-- Versión de PHP: 7.3.9
 
 SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
 SET AUTOCOMMIT = 0;
@@ -21,6 +21,28 @@ SET time_zone = "+00:00";
 --
 -- Base de datos: `grupo22`
 --
+
+-- --------------------------------------------------------
+
+--
+-- Estructura de tabla para la tabla `config`
+--
+
+CREATE TABLE `config` (
+  `id` int(11) NOT NULL,
+  `titulo` varchar(255) COLLATE utf8_spanish_ci NOT NULL,
+  `descripcion` text COLLATE utf8_spanish_ci NOT NULL,
+  `contacto` varchar(255) COLLATE utf8_spanish_ci NOT NULL,
+  `paginacion` int(100) NOT NULL,
+  `sitio_habilitado` tinyint(1) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_spanish_ci;
+
+--
+-- Volcado de datos para la tabla `config`
+--
+
+INSERT INTO `config` (`id`, `titulo`, `descripcion`, `contacto`, `paginacion`, `sitio_habilitado`) VALUES
+(1, 'Orquesta Escuela de Berisso', 'Lorem Ipsum es simplemente el texto de relleno de las imprentas y archivos de texto. Lorem Ipsum ha sido el texto de relleno estándar de las industrias desde el año 1500, cuando un impresor (N. del T. persona que se dedica a la imprenta) desconocido usó una galería de textos y los mezcló de tal manera que logró hacer un libro de textos especimen. No sólo sobrevivió 500 años, sino que tambien ingresó como texto de relleno en documentos electrónicos, quedando esencialmente igual al original. Fue popularizado en los 60s con la creación de las hojas \"Letraset\", las cuales contenian pasajes de Lorem Ipsum, y más recientemente con software de autoedición, como por ejemplo Aldus PageMaker, el cual incluye versiones de Lorem Ipsum.', 'Teléfono: 2216598741 // 4578996', 3, 1);
 
 -- --------------------------------------------------------
 
@@ -139,6 +161,30 @@ CREATE TABLE `usuario` (
   `last_name` varchar(255) NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
 
+--
+-- Volcado de datos para la tabla `usuario`
+--
+
+INSERT INTO `usuario` (`id`, `email`, `username`, `password`, `activo`, `updated_at`, `created_at`, `first_name`, `last_name`) VALUES
+(1, 'admin@test.com', 'admin_principal', '123456', 1, '2019-10-23 20:40:44', '2019-10-23 20:40:44', 'Admin', 'Admin'),
+(2, 'test1@test.com', 'test1', '123456', 1, '2019-10-23 20:50:05', '2019-10-23 20:50:05', 'Estudiante1', 'Estudiante1'),
+(3, 'test2@test.com', 'test2', '123456', 1, '2019-10-23 20:50:34', '2019-10-23 20:50:34', 'Estudiante2', 'Estudiante2'),
+(4, 'test3@test.com', 'test3', '123456', 1, '2019-10-23 20:50:54', '2019-10-23 20:50:54', 'Estudiante3', 'Estudiante3'),
+(5, 'test4@test.com', 'test4', '123456', 1, '2019-10-23 20:51:08', '2019-10-23 20:51:08', 'Estudiante4', 'Estudiante4'),
+(6, 'test5@test.com', 'test5', '123456', 1, '2019-10-23 20:51:28', '2019-10-23 20:51:28', 'Estudiante5', 'Estudiante5'),
+(7, 'test6@test.com', 'test6', '123456', 1, '2019-10-23 20:52:01', '2019-10-23 20:52:01', 'Preceptor1', 'Preceptor1'),
+(8, 'test7@test.com', 'test7', '123456', 1, '2019-10-23 20:52:13', '2019-10-23 20:52:13', 'Preceptor2', 'Preceptor2'),
+(9, 'test8@test.com', 'test8', '123456', 1, '2019-10-23 20:54:18', '2019-10-23 20:54:18', 'Preceptor3', 'Preceptor3'),
+(10, 'test9@test.com', 'test9', '123456', 1, '2019-10-23 20:54:56', '2019-10-23 20:54:56', 'Docente1', 'Docente1'),
+(11, 'test10@test.com', 'test10', '123456', 1, '2019-10-23 20:55:08', '2019-10-23 20:55:08', 'Docente2', 'Docente2'),
+(12, 'test11@test.com', 'test11', '123456', 1, '2019-10-23 20:55:22', '2019-10-23 20:55:22', 'Docente3', 'Docente3'),
+(13, 'test12@test.com', 'test12', '123456', 1, '2019-10-23 20:55:34', '2019-10-23 20:55:34', 'Docente4', 'Docente4'),
+(14, 'test13@test.com', 'test13', '123456', 1, '2019-10-23 20:56:26', '2019-10-23 20:56:26', 'Preceptor y Docente 1', 'Preceptor y Docente 1'),
+(15, 'test14@test.com', 'test14', '123456', 1, '2019-10-23 20:56:45', '2019-10-23 20:56:45', 'Preceptor y Docente 2', 'Preceptor y Docente 2'),
+(16, 'test15@test.com', 'test15', '123456', 1, '2019-10-23 20:57:10', '2019-10-23 20:57:10', 'Estudiante y Docente 1', 'Estudiante y Docente 1'),
+(17, 'test16@test.com', 'test16', '123456', 1, '2019-10-23 20:57:26', '2019-10-23 20:57:26', 'Estudiante y Docente 2', 'Estudiante y Docente 2'),
+(18, 'test17@test.com', 'test17', '123456', 1, '2019-10-23 20:58:07', '2019-10-23 20:58:07', 'Admin y Preceptor 1', 'Admin y Preceptor 1');
+
 -- --------------------------------------------------------
 
 --
@@ -151,8 +197,43 @@ CREATE TABLE `usuario_tiene_rol` (
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
 
 --
+-- Volcado de datos para la tabla `usuario_tiene_rol`
+--
+
+INSERT INTO `usuario_tiene_rol` (`usuario_id`, `rol_id`) VALUES
+(1, 1),
+(2, 4),
+(3, 4),
+(4, 4),
+(5, 4),
+(6, 4),
+(7, 2),
+(8, 2),
+(9, 2),
+(10, 3),
+(11, 3),
+(12, 3),
+(13, 3),
+(14, 2),
+(14, 3),
+(15, 2),
+(15, 3),
+(16, 3),
+(16, 4),
+(17, 3),
+(17, 4),
+(18, 1),
+(18, 2);
+
+--
 -- Índices para tablas volcadas
 --
+
+--
+-- Indices de la tabla `config`
+--
+ALTER TABLE `config`
+  ADD PRIMARY KEY (`id`);
 
 --
 -- Indices de la tabla `permiso`
@@ -177,6 +258,12 @@ ALTER TABLE `usuario`
 --
 
 --
+-- AUTO_INCREMENT de la tabla `config`
+--
+ALTER TABLE `config`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=2;
+
+--
 -- AUTO_INCREMENT de la tabla `permiso`
 --
 ALTER TABLE `permiso`
@@ -192,7 +279,7 @@ ALTER TABLE `rol`
 -- AUTO_INCREMENT de la tabla `usuario`
 --
 ALTER TABLE `usuario`
-  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT;
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=19;
 COMMIT;
 
 /*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
